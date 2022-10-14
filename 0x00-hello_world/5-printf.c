e999f510d0ea0b1211af637270bc687129535946
Adebank <adebankepeke04@gmail.com>
2022-10-14 04:12:45 -0700

    task 6 and 7

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main-entry point for the program.
+ * main-entry point for the program
  *
- * return:0 if no errors, return non zero if errors
+ * return:0 if no errors
  */
-int main(void)
+int main(vioid)
 {
 	print f("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
