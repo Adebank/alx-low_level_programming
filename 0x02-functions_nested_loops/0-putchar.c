c6934a9c339deb7065552b5263ac6525a323c75c
Adebank <adebankepeke04@gmail.com>
2022-10-19 04:50:38 -0700

    task_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
+#include <unistd.h>
 #include "main.h"
 /**
  * main - prints _putchar and a new line
  * is written by adebanke
- * Return:0
+ * Return: 0
  **/
 int main(void)
 {
+	char word[8] = "_putchar";
+	int i;
 
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
 	_putchar('\n');
+
 	return (0);
 }
