29f08cce6f314afbd05eedcf7570466cb80899ee
Adebank <adebankepeke04@gmail.com>
2022-10-24 14:33:52 -0700

    task_2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the value of two integers.
+ * @a:first integer to be swapped
+ * @b:second integer to be swapped
+ * Return:Void
+ **/
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+		*a = *b;
+		*b = p;
+}
